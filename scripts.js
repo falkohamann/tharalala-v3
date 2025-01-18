aa44b187ca998c2240e02b980c73f2044be101b1
Falko Hamann <falko.hamann@sap.com>
2025-01-18 20:35:36 +0100

    before footer / header split

--- scripts.js
+++ scripts.js
@@ -73,3 +73,16 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     };
 });
+// Load header content
+fetch('header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  });
+
+// Load footer content
+fetch('footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  });
